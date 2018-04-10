1f56556f568babd96b6e1611d2b4c66558f8d4c0
Zhao Li <dev@lizhao.net>
2018-04-11 06:43:07 +0800

    fixed json parsing

--- docs/static/js/18.5b38bffb9ea7a9e883dd.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([18],{wrTU:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Party")])},staticRenderFns:[]},a=n("VU/8")({name:"partyPage"},r,!1,null,null,null);t.default=a.exports}});
