350ffaa988a17cd4ff1befa57956619e257b66a8
Zhao Li <dev@lizhao.net>
2018-04-16 16:23:07 +0800

    revamp

--- docs/js/contact-form.js
+++ docs/js/contact-form.js
@@ -14,4 +14,59 @@ Ajax Contact Form
 (function ($, window, document, undefined) {
     'use strict';
 
+    var $form = $('#contact-form');
+
+    $form.submit(function (e) {
+        // remove the error class
+        $('.form-group').removeClass('has-error');
+        $('.help-block').remove();
+
+        // get the form data
+        var formData = {
+            'name' : $('input[name="formName"]').val(),
+            'email' : $('input[name="formEmail"]').val(),
+            'subject' : $('input[name="formSubject"]').val(),
+            'message' : $('textarea[name="formMessage"]').val()
+        };
+
+        // process the form
+        $.ajax({
+            type : 'POST',
+            url  : 'http://app.traco.co:4000/mail',
+            data : formData,
+            dataType : 'json',
+            encode : true
+        }).done(function (data) {
+            // handle errors
+            if (!data.success) {
+                if (data.errors.name) {
+                    $('#name-field').addClass('has-error');
+                    $('#name-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.name + '</span>');
+                }
+
+                if (data.errors.email) {
+                    $('#email-field').addClass('has-error');
+                    $('#email-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.email + '</span>');
+                }
+
+                if (data.errors.subject) {
+                    $('#subject-field').addClass('has-error');
+                    $('#subject-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.subject + '</span>');
+                }
+
+                if (data.errors.message) {
+                    $('#message-field').addClass('has-error');
+                    $('#message-field').find('.col-lg-10').append('<span class="help-block">' + data.errors.message + '</span>');
+                }
+            } else {
+                // display success message
+                $form.html('<div class="alert alert-success">' + data.message + '</div>');
+            }
+        }).fail(function (data) {
+            // for debug
+            console.log(data)
+        });
+
+        e.preventDefault();
+    });
 }(jQuery, window, document));
